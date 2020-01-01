53cd37e1e0e81fcb607a6149581fa02873977c55
Bell <bell04204@gmail.com>
2020-01-01 19:51:17 +0900

    CI/CD 설정 수정..2, API JSON 테스트
    deploy stage를 local 파일로 실행하게 수정.
    제발 한번에 됐으면 좋겠네

--- src/main/java/net/softbell/bsh/dto/test/APITestDTO.java
+++ src/main/java/net/softbell/bsh/dto/test/APITestDTO.java
@@ -1,13 +1,33 @@
 package net.softbell.bsh.dto.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class APITestDTO {
 	private String strName;
 	private Integer intAge;
+	private List<ExampleClass> values;
+	
+	public APITestDTO(String strName, Integer intAge)
+	{
+		this.strName = strName;
+		this.intAge = intAge;
+		values = new ArrayList<ExampleClass>();
+		for (int i = 1; i <= 3; i++)
+			values.add(new ExampleClass("Test", i));
+	}
+	
+	@AllArgsConstructor
+	@Getter
+	@Setter
+	class ExampleClass {
+		private String strFirst;
+		private Integer intSecond;
+	}
 }
