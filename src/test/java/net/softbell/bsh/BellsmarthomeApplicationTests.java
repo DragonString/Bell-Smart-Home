d02966e63c9be9325bf942d5adcfb7d84e234914
Bell <bell04204@gmail.com>
2020-01-16 19:00:10 +0900

    흠 되려나

--- src/test/java/net/softbell/bsh/BellsmarthomeApplicationTests.java
+++ src/test/java/net/softbell/bsh/BellsmarthomeApplicationTests.java
@@ -4,22 +4,24 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import lombok.extern.slf4j.Slf4j;
 import net.softbell.bsh.util.BellLog;
 
 /**
  * @Author : Bell(bell@softbell.net)
  * @Description : Spring Boot Test 진입지점
  */
-@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BellsmarthomeApplicationTests
 {
 	// Global Field
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 	@Test
 	public void contextLoads() throws Exception
 	{
