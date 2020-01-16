d14bd882d79e9e9e2cb77b61dcbda62942976bae
Bell <bell04204@gmail.com>
2020-01-16 09:10:06 +0900

    DevOps 테스트중 환경변수 어떻게 나오는지 점검

--- src/test/java/net/softbell/bsh/BellsmarthomeApplicationTests.java
+++ src/test/java/net/softbell/bsh/BellsmarthomeApplicationTests.java
@@ -4,28 +4,27 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
 import net.softbell.bsh.util.BellLog;
 
 /**
  * @Author : Bell(bell@softbell.net)
  * @Description : Spring Boot Test 진입지점
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BellsmarthomeApplicationTests {
+public class BellsmarthomeApplicationTests
+{
 	// Global Field
-	private final Logger G_Logger = LoggerFactory.getLogger(this.getClass());
-
 	@Test
-	public void contextLoads() throws Exception {
-		G_Logger.info(BellLog.getLogHead() + "Test Starting..");
+	public void contextLoads() throws Exception
+	{
+		log.info(BellLog.getLogHead() + "Test Starting..");
 		assertEquals(true, true); // 두 값이 다르면 Test 스테이지가 중단됨.
-		G_Logger.info(BellLog.getLogHead() + "Test Complete!!");
+		log.info(BellLog.getLogHead() + "Test Complete!!");
 	}
-
 }
