dc3f4d06815ffa11708f2baf08591604f8d2f768
Bell <bell04204@gmail.com>
2020-01-18 02:15:07 +0900

    노드 관련 기능 추가
    - 노드 정보 수정기능 추가
    - 노드 아이템 정보 수정기능 추가
    - 모니터 기능 추가

--- src/main/java/net/softbell/bsh/controller/view/AdminNodeView.java
+++ src/main/java/net/softbell/bsh/controller/view/AdminNodeView.java
@@ -4,6 +4,8 @@ import java.security.Principal;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.softbell.bsh.domain.EnableStatusRule;
 import net.softbell.bsh.domain.entity.Member;
+import net.softbell.bsh.domain.entity.Node;
 import net.softbell.bsh.iot.service.v1.IotNodeServiceV1;
 import net.softbell.bsh.service.MemberService;
 import net.softbell.bsh.util.BellLog;
@@ -27,12 +30,13 @@ import net.softbell.bsh.util.BellLog;
 public class AdminNodeView
 {
 	// Global Field
+	private final String G_BASE_PATH = "services/admin";
 	private final String G_BASE_REDIRECT_URL = "redirect:/admin/node";
 	private final String G_LOGOUT_REDIRECT_URL = "redirect:/logout";
 	private final MemberService memberService;
 	private final IotNodeServiceV1 iotNodeService;
 	
-	// 회원 승인 처리
+	// 노드 비활성화 처리
     @PostMapping("disable")
     public String procNodeDisable(Model model, Principal principal,
     		@RequestParam("intNodeId") int intNodeId)
@@ -54,7 +58,7 @@ public class AdminNodeView
     		return G_BASE_REDIRECT_URL + "?error";
     }
     
-	// 회원 승인 처리
+	// 노드 활성화 처리
     @PostMapping("enable")
     public String procNodeEnable(Model model, Principal principal,
     		@RequestParam("intNodeId") int intNodeId)
@@ -76,7 +80,7 @@ public class AdminNodeView
     		return G_BASE_REDIRECT_URL + "?error";
     }
     
-    // 회원 승인 처리
+    // 노드 제한 처리
     @PostMapping("reject")
     public String procNodeReject(Model model, Principal principal,
     		@RequestParam("intNodeId") int intNodeId)
@@ -96,5 +100,66 @@ public class AdminNodeView
         	return G_BASE_REDIRECT_URL;
     	else
     		return G_BASE_REDIRECT_URL + "?error";
+    }
+    
+    // 노드 정보 수정페이지 출력
+    @GetMapping("modify/{id}")
+    public String dispNodeModify(Model model, Principal principal, @PathVariable("id") long nodeId)
+    {
+    	// Field
+    	Member member = memberService.getAdminMember(principal.getName());
+		Node node;
+		
+		// Init
+		node = iotNodeService.getNode(nodeId);
+    	
+    	// Exception
+    	if (member == null)
+    		return G_LOGOUT_REDIRECT_URL;
+    	
+    	// Process
+    	model.addAttribute("nodeInfo", node);
+		
+		// Return
+        return G_BASE_PATH + "/NodeModify";
+    }
+    
+    // 노드 정보 수정 프로세스 수행
+    @PostMapping("modify/{id}")
+    public String procNodeModify(Model model, Principal principal, @PathVariable("id") long nodeId,
+    							@RequestParam("alias") String alias)
+    {
+    	// Field
+    	Member member = memberService.getAdminMember(principal.getName());
+    	
+    	// Exception
+    	if (member == null)
+    		return G_LOGOUT_REDIRECT_URL;
+    	
+    	// Process
+    	if (iotNodeService.setNodeAlias(nodeId, alias))
+    		return G_BASE_REDIRECT_URL + "/modify/" + nodeId;
+    	else
+    		return G_BASE_REDIRECT_URL + "/modify/" + nodeId + "?error";
+    }
+    
+    // 노드 정보 수정 프로세스 수행
+    @PostMapping("modify/item/{id}")
+    public String procNodeItemModify(Model model, Principal principal, @PathVariable("id") long nodeItemId,
+    							@RequestParam("id") long nodeId,
+    							@RequestParam("alias") String alias)
+    {
+    	// Field
+    	Member member = memberService.getAdminMember(principal.getName());
+    	
+    	// Exception
+    	if (member == null)
+    		return G_LOGOUT_REDIRECT_URL;
+    	
+    	// Process
+    	if (iotNodeService.setNodeItemAlias(nodeItemId, alias))
+    		return G_BASE_REDIRECT_URL + "/modify/" + nodeId;
+    	else
+    		return G_BASE_REDIRECT_URL + "/modify/" + nodeId + "?error";
     }
 }
