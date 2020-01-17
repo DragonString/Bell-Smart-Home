d859a3547a93d9303bba9d62e63988a25c7459e8
Bell <bell04204@gmail.com>
2020-01-18 03:51:28 +0900

    액션 기능 추가
    - 액션 서비스 구현중
    - 액션 뷰 구현중 틀짜놓음
    - 모든 뷰 컨텐츠 부분에 헤딩부분 추가함

--- src/main/java/net/softbell/bsh/controller/view/ActionView.java
+++ src/main/java/net/softbell/bsh/controller/view/ActionView.java
@@ -1,23 +1,49 @@
 package net.softbell.bsh.controller.view;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import net.softbell.bsh.domain.entity.NodeItem;
+import net.softbell.bsh.iot.service.v1.IotActionServiceV1;
 
 /**
  * @Author : Bell(bell@softbell.net)
  * @Description : 모니터 뷰 컨트롤러
  */
+@AllArgsConstructor
 @Controller
 @RequestMapping("/action")
 public class ActionView
 {
 	// Global Field
 	private final String G_BASE_PATH = "services/general";
+	private final IotActionServiceV1 iotActionService;
 	
 	@GetMapping()
     public String dispIndex()
 	{
         return G_BASE_PATH + "/Action";
+    }
+	
+	@GetMapping("/create")
+    public String dispAdd(Model model, Authentication auth)
+	{
+		// Field
+		List<NodeItem> listNodeItem;
+		
+		// Init
+		listNodeItem = iotActionService.getAvailableNodeItem(auth);
+		
+		// Process
+		model.addAttribute("listNodeItem", listNodeItem);
+		
+		// Return
+        return G_BASE_PATH + "/ActionCreate";
     }
 }
--- src/main/java/net/softbell/bsh/iot/service/v1/IotActionServiceV1.java
+++ src/main/java/net/softbell/bsh/iot/service/v1/IotActionServiceV1.java
@@ -1,8 +1,13 @@
 package net.softbell.bsh.iot.service.v1;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
+import net.softbell.bsh.domain.entity.NodeItem;
+import net.softbell.bsh.domain.repository.NodeItemRepo;
 import net.softbell.bsh.iot.component.v1.IotAuthCompV1;
 import net.softbell.bsh.iot.component.v1.IotChannelCompV1;
 
@@ -17,6 +22,18 @@ public class IotActionServiceV1
 	// Global Field
 	private final IotChannelCompV1 iotChannelCompV1;
 	private final IotAuthCompV1 iotAuthCompV1;
+	private final NodeItemRepo nodeItemRepo;
 	
-	
+	public List<NodeItem> getAvailableNodeItem(Authentication auth)
+	{
+		// Field
+		List<NodeItem> listNodeItem;
+		
+		// TODO 계정에서 접근가능한 아이템만 반환하도록 추가해야됨.. 나중에... 언젠가는 추가하겠지...?
+		// Init
+		listNodeItem = nodeItemRepo.findAll();
+		
+		// Return
+		return listNodeItem;
+	}
 }
