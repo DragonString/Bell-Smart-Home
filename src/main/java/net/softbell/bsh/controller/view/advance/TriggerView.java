1652af0a3a4f32f0668a3fefc03a5aedadf03463
Bell <bell04204@gmail.com>
2020-01-23 18:08:30 +0900

    트리거 뷰 틀 틀 마련

--- src/main/java/net/softbell/bsh/controller/view/advance/TriggerView.java
+++ src/main/java/net/softbell/bsh/controller/view/advance/TriggerView.java
@@ -5,16 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.AllArgsConstructor;
+import net.softbell.bsh.iot.service.v1.IotTriggerServiceV1;
+import net.softbell.bsh.service.ViewDtoConverterService;
+
 /**
  * @Author : Bell(bell@softbell.net)
  * @Description : 트리거 뷰 컨트롤러
  */
+@AllArgsConstructor
 @Controller
 @RequestMapping("/trigger")
 public class TriggerView
 {
 	// Global Field
 	private final String G_BASE_PATH = "services/advance";
+	private final ViewDtoConverterService viewDtoConverterService;
+	private final IotTriggerServiceV1 iotTriggerService;
 	
 	@GetMapping()
     public String dispIndex(Model model)
